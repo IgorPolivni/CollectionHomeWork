a5bf3a82c9319a0c98c60532e692d4dbb83bf475
Poli_tn62@student.itstep.kz <Poli_tn62@student.itstep.kz>
2021-02-14 22:05:19 +0600

    Добавьте файлы проекта.

---
+++ CollectionHomeWork/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionHomeWork
+{
+    public class MyList : List<int>
+    {
+        public new void Sort()
+        {
+            if (Count > 1)
+            {
+                int count = new int();
+                do
+                {
+                    count = new int();
+                    for (int i = 0; i < Count - 1; i++)
+                    {
+                        if (this[i] > this[i + 1])
+                        {
+                            count++;
+                            int temp = this[i];
+                            this[i] = this[i + 1];
+                            this[i + 1] = temp;
+                        }
+                    }
+                } while (count != 0);
+            }
+        }
+    }
+}
---
+++ CollectionHomeWork/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Random random = new Random();
+            var list = new List<int>();
+            const int sizeArray = 20;
+            for (int i = 0; i < sizeArray; i++)
+            {
+                list.Add(random.Next(1, 10));
+            }
+
+            Console.WriteLine("Все элементы Листа: ");
+            foreach (var element in list)
+            {
+                Console.Write($"{element} ");
+            }
+
+            int maxValue = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > maxValue)
+                {
+                    maxValue = list[i];
+                }
+            }
+
+            int preMaxValue = list[0];
+            int maxPosition = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > preMaxValue && list[i] != maxValue)
+                {
+                    preMaxValue = list[i];
+                    maxPosition = i;
+                }
+            }
+            Console.WriteLine($"\nВторое максимальное значение в коллекции: Позиция = {maxPosition } и значение элемента = {preMaxValue}");
+
+            Console.ReadLine( );
+            Console.Clear();
+            //Задание 2
+            MyList myList = new MyList();
+            for (int i = 0; i < sizeArray; i++)
+            {
+                myList.Add(random.Next(1, 10));
+            }
+            Console.WriteLine("Все элементы Листа: ");
+            foreach (var element in myList)
+            {
+                Console.Write($"{element} ");
+            }
+            myList.Sort();
+            Console.WriteLine("\nВсе элементы Листа после сортировки: ");
+            foreach (var element in myList)
+            {
+                Console.Write($"{element} ");
+            }
+
+
+
+        }
+
+    }
+}
